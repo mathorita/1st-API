755ce1ea29c2e7ba96e084b247763a5672e63efa
Matheus <matshiaki@gmail.com>
2025-05-09 10:03:42 +0900

    owner added

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner_username')
     class Meta:
         model = Task
         fields = '__all__'
--- tasks/views.py
+++ tasks/views.py
@@ -4,10 +4,16 @@ from .serializers import TaskSerializer
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    def get_queryset(self, serializer):
+        return Task.objects.filter(owner=self.request.user)
 
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['is_completed']
